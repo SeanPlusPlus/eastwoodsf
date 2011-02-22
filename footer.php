668d5b240183b85c87d96771dfcb36174b01b136
Sean Stephenson <seanmichaelstephenson@gmail.com>
2011-02-21 22:25:15 -0800

    added files from my wordpress theme

---
+++ footer.php
@@ -0,0 +1,18 @@
+	</div><!-- #main -->
+	
+	<div id="footer">
+		<div id="colophon">
+		
+			<div id="site-info">
+				<p>Powered by <span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'your-theme' ) ?>" rel="generator"><?php _e( 'WordPress', 'your-theme' ) ?></a></span> and <span id="theme-link"><a href="http://brownbagwebdesign.com/" title="<?php _e( 'Brown Bag Web Design', 'bbwd' ) ?>" rel="designer"><?php _e( 'brown bag web design', 'BBWD' ) ?></a></span>.</p>			
+			</div><!-- #site-info -->
+			
+		</div><!-- #colophon -->
+	</div><!-- #footer -->
+	
+</div><!-- #wrapper -->	
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
