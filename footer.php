a1d241b9b7fac371fb35b237020170cdeb6f4314
Sean Stephenson <seanmichaelstephenson@gmail.com>
2011-03-05 19:14:52 -0800

    simplify footer

--- footer.php
+++ footer.php
@@ -1,13 +1,10 @@
 	</div><!-- #main -->
 	
 	<div id="footer">
-		<div id="colophon">
+		<p>Eastwood Development</p>
+		<p>Contact</p>
 		
-			<div id="site-info">
-				<p>Powered by <span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'your-theme' ) ?>" rel="generator"><?php _e( 'WordPress', 'your-theme' ) ?></a></span> and <span id="theme-link"><a href="http://brownbagwebdesign.com/" title="<?php _e( 'Brown Bag Web Design', 'bbwd' ) ?>" rel="designer"><?php _e( 'brown bag web design', 'BBWD' ) ?></a></span>.</p>			
-			</div><!-- #site-info -->
-			
-		</div><!-- #colophon -->
+
 	</div><!-- #footer -->
 	
 </div><!-- #wrapper -->	
